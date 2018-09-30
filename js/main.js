f75647e48ecd8f2a4469d2c266a80aca8f442a72
Lea Louesdon <lea.louesdon@gmail.com>
2018-09-30 17:08:24 +0200

    modifs

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var selected = false;
 var reponse=0;
 var sourceQuestions = "../src/Questions.txt";
 var sourceCalcTemp = "../src/QRef.txt";
-var equiH = "../src/H_RVal.txt.txt";
+var equiH = "../src/H_RVal.txt";
 var equiF = "../src/F_RVal.txt";
 var sexe; //1=Femme 2=Homme
 
@@ -175,8 +175,8 @@ function select() {
 /*function click(val) {
   if(val==1){
     //$( "#rep1" ).slideUp( 300 ).delay( 800 ).fadeIn( 400 );
-    $( "#rep1" ).animate({zoom: '150%'}, "fast");
-    $( "#rep1" ).animate({zoom: '100%'}, "fast");
+    //$( "#rep1" ).animate({zoom: '150%'}, "fast");
+    //$( "#rep1" ).animate({zoom: '100%'}, "fast");
   }else if (val==2) {
     $( "#rep2" ).animate({zoom: '150%'}, "fast");
     $( "#rep2" ).animate({zoom: '100%'}, "fast");
@@ -256,24 +256,14 @@ function calc() {
   console.log(evitement);
   console.log(distruction);
   console.log(diversion);
-  valuesGraph(tache,emotion,evitement,distruction,diversion);
+  valuesGraph();
   $(".overlay").show();
 }
 
 /*values for graph*/
 var equi = []; //equivalence score graph
-function valuesGraph(tache,emotion,evitement,distruction,diversion) {
-  var gTache = 0;
-  var gEmotion = 0;
-  var gEvitement = 0;
-  var gDistruction = 0;
-  var gDiversion = 0;
-
-
-  setGraph(tache,emotion,evitement,distruction,diversion);
-}
-
-function initEqui() {
+function valuesGraph() {
+  //initEqui();
   /*if(sexe==1){
 
   }else{
@@ -284,23 +274,51 @@ function initEqui() {
       var res = result.split("\n");
       readEqui(res);
   });
+
 }
+
 
 function readEqui(res) {
   var i;
   var arrayOfStrings;
-  var temp = [];
   for (i = 0; i < res.length; i++) {
       arrayOfStrings = res[i].split(";");
-      temp = [];
-      temp.push(arrayOfStrings[1]);
-      temp.push(arrayOfStrings[2]);
-      temp.push(arrayOfStrings[3]);
-      temp.push(arrayOfStrings[4]);
-      temp.push(arrayOfStrings[5]);
-      equi.push(temp);
+      equi.push(arrayOfStrings);
   }
-  calc();
+  calcEqui();
+}
+
+function calcEqui() {
+  var gTache = 0;
+  var gEmotion = 0;
+  var gEvitement = 0;
+  var gDistruction = 0;
+  var gDiversion = 0;
+
+
+  var temp = [];
+  for (i=0; i<equi.length; i++){
+    temp = equi[i];
+    if(temp[2]==tache){
+      gTache = temp[1];
+    }else if (temp[3]==emotion) {
+      gEmotion = temp[1];
+    }else if (temp[4]==evitement) {
+      gEvitement = temp[1];
+    }else if (temp[5]==distruction) {
+      gDistruction = temp[1];
+    }else if (temp[6]==diversion) {
+      gDiversion = temp[1];
+    }
+  }
+
+  console.log(gTache);
+  console.log(gEmotion);
+  console.log(gEvitement);
+  console.log(gDistruction);
+  console.log(gDiversion);
+  setGraph(gTache,gEmotion,gEvitement,gDistruction,gDiversion);
+
 }
 
 /*graph*/
